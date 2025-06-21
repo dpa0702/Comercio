ee662f3f6c7f7a5d76a4eebc68f72ed964ffadf5
dpa0702 <56276309+dpa0702@users.noreply.github.com>
2025-06-21 20:35:49 -0300

    Evolução relatórios.

--- src/app/pages/relatorios/relatorios.component.ts
+++ src/app/pages/relatorios/relatorios.component.ts
@@ -12,6 +12,7 @@ import { MatSortModule } from '@angular/material/sort';
 import { getPortuguesePaginatorIntl } from '../../components/mat-paginator-intl-pt/mat-paginator-intl-pt';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MeiosPagamentoService } from '../../services/meios-pagamento.service';
+import { PedidoService } from '../../services/pedido.service';
 
 @Component({
   selector: 'app-relatorios',
@@ -40,10 +41,18 @@ export class RelatoriosComponent {
   dataSource = new MatTableDataSource<any>([]);
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor(private meiosPagamentoService: MeiosPagamentoService,) {}
+  displayedColumns2: string[] = ['id', 'clienteNome', 'total'];
+  dataSource2 = new MatTableDataSource<any>([]);
+  @ViewChild(MatPaginator) paginator2!: MatPaginator;
+
+  constructor(
+    private meiosPagamentoService: MeiosPagamentoService,
+    private pedidosService: PedidoService
+  ) {}
 
   ngOnInit(): void {
     this.carregarMeiosPagamento();
+    this.carregarAprazoDia();
   }
 
   carregarMeiosPagamento(): void {
@@ -51,6 +60,13 @@ export class RelatoriosComponent {
       this.dataSource.data = data;
       this.dataSource.paginator = this.paginator;
     });
+  }
+
+  carregarAprazoDia(): void{
+    this.pedidosService.listarAPrazoDia().subscribe(data => {
+      this.dataSource2.data = data;
+      this.dataSource2.paginator = this.paginator2;
+    })
   }
 
 }
